13948c5c53f43c0af2f8ddcb62a0d71bc04930e5
LUIS AMIR AQUINO MEJÍA <la2023-0685@uce.edu.do>
2026-01-28 19:50:50 -0400

    1.1 Cree una clase aparte que tiene mi excepcion personalizada

---
+++ Proyect/ErrorRedException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyect
+{
+        public class ErrorRedException : Exception
+        {
+            public ErrorRedException(string mensaje) : base(mensaje) { }
+        }
+}
