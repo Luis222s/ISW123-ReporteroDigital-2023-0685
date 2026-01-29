08831bf2bf6055ac4e2b79bf8561a1eed980e902
LUIS AMIR AQUINO MEJÍA <la2023-0685@uce.edu.do>
2026-01-28 20:25:51 -0400

    1.6 Cree una clase que maneja los eventos y excepciones, esta clase instancia mis clases que estan en la carpeta servicio y luego los metodos que estan en esas clases para luego esperar con el await Task whenall a que los metodos terminen sus tareas en el tiempo que le puse y luego suscribimos a "FuenteProcesada" para que diga el texto cuando termine la espera de cada metodo, al final imprimo el resultado que me dio cada metodo

---
+++ Proyect/Reportero.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Proyect.Servicios;
+
+namespace Proyect
+{
+    public class Reportero
+    {
+        public event Action<string> FuenteProcesada;
+
+        public async Task CrearArticuloAsync()
+        {
+            var textoSrv = new ServicioTexto();
+            var imgSrv = new ServicioImagenes();
+            var analisisSrv = new ServicioAnalisis();
+
+            try
+            {
+                var tareaTexto = textoSrv.ObtenerTextoAsync();
+                var tareaImg = imgSrv.ObtenerImagenesAsync();
+                var tareaAnalisis = analisisSrv.ObtenerAnalisisAsync();
+
+                await Task.WhenAll(tareaTexto, tareaImg, tareaAnalisis);
+
+                FuenteProcesada?.Invoke("Texto listo");
+                FuenteProcesada?.Invoke("Imágenes listas");
+                FuenteProcesada?.Invoke("Análisis listo");
+
+                Console.WriteLine("\n ARTÍCULO FINAL:\n");
+                Console.WriteLine(tareaTexto.Result);
+                Console.WriteLine(tareaImg.Result);
+                Console.WriteLine(tareaAnalisis.Result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
