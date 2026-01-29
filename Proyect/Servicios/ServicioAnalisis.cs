783b7e6f86e74a2910ec3796ab873f76b41343e7
LUIS AMIR AQUINO MEJÍA <la2023-0685@uce.edu.do>
2026-01-28 20:16:43 -0400

    1.5 Esta clase simula un analisis de datos y se ejecuta en paralelo con las demas fuentes/servicios

---
+++ Proyect/Servicios/ServicioAnalisis.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyect.Servicios
+{
+    public class ServicioAnalisis
+    {
+        public async Task<string> ObtenerAnalisisAsync()
+        {
+            await Task.Delay(2500);
+            return "Análisis: Expertos opinan que el crecimiento es sostenible.";
+        }
+    }
+}
