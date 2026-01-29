2bda5ead7353b355dff0a703c02599aa58816326
LUIS AMIR AQUINO MEJÍA <la2023-0685@uce.edu.do>
2026-01-28 20:36:01 -0400

    1.7 mi clase Program lo que hace es instanciar mi clase Reportero y llama "FuenteProcesada" de esa clase luego espera que termine el metodo para imprimir "Proceso finalizado" y terminar el programa

--- Proyect/Program.cs
+++ Proyect/Program.cs
@@ -1,1 +1,20 @@
 ﻿using System;
+using Proyect;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var reportero = new Reportero();
+
+        reportero.FuenteProcesada += mensaje =>
+        {
+            Console.WriteLine($"Evento: {mensaje}");
+        };
+
+        await reportero.CrearArticuloAsync();
+
+        Console.WriteLine("\nProceso finalizado. Presione una tecla...");
+        Console.ReadKey();
+    }
+}
