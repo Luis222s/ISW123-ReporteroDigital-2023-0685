32966c45076dc1701ae1f4963fe1e16587ccdfcd
LUIS AMIR AQUINO MEJÍA <la2023-0685@uce.edu.do>
2026-01-28 20:08:28 -0400

    1.3 Cree otra carpeta llamada Servicios, esta clase simula una fuente externa de texto

---
+++ Proyect/Servicios/ServicioTexto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Proyect.Excepciones; 
+
+namespace Proyect.Servicios
+{
+    public class ServicioTexto
+    {
+        public async Task<string> ObtenerTextoAsync()
+        {
+            await Task.Delay(2000); 
+
+            if (new Random().Next(1, 5) == 1)
+                throw new ErrorRedException("Error al obtener el texto de la noticia.");
+
+            return "Noticia: Economía dominicana crece un 5%.";
+        }
+    }
+}
