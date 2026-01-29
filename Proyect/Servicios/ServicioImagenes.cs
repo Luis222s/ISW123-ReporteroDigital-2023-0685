3583b22a8ecd4af6ff2e2997b4d28757bb1a7f17
LUIS AMIR AQUINO MEJÍA <la2023-0685@uce.edu.do>
2026-01-28 20:10:52 -0400

    1.4 Esta clase simula una fuente externa de imagenes se estan cargando

---
+++ Proyect/Servicios/ServicioImagenes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyect.Servicios
+{
+    public class ServicioImagenes
+    {
+        public async Task<string> ObtenerImagenesAsync()
+        {
+            await Task.Delay(1500);
+            return "Imagenes cargadas correctamente.";
+        }
+    }
+    
+}
